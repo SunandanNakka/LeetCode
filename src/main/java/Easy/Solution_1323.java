01c81d035eb2dac1f3c37347f271c1ec0d4f9161
Sunandan-Nakka <sunandan.nakka@thermofisher.com>
2022-03-13 22:58:06 +0530

    Added description for Solution_1323.

--- src/main/java/Easy/Solution_1323.java
+++ src/main/java/Easy/Solution_1323.java
@@ -4,6 +4,11 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+/* 1323. Maximum 69 Number
+
+    You are given a positive integer num consisting only of digits 6 and 9.
+    Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+*/
 public class Solution_1323 {
     private static final Logger logger = LogManager.getLogger(Solution_1323.class);
 
